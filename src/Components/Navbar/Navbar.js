30541b419111f06acb3e7f960cd10593cfc01cb8
keisukeisobe <52838946+keisukeisobe@users.noreply.github.com>
2020-08-17 17:15:03 -0400

    navbar now appears on scroll down-- will have to adjust scrolldown height

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,36 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
-// const classNames = require('classnames');
+import './Navbar.css';
+const classNames = require('classnames');
 
 function Navbar() {
-  const [state, setState] = useState('closed');
+  const [closed, setClosed] = useState(true);
+  const [scrollPosition, setScrollPosition] = useState(window.pageYOffset);
+
+  const handleScrollPosition = () => {
+    setScrollPosition(window.pageYOffset);
+    console.log(scrollPosition);
+    if (scrollPosition > 100){
+      setClosed(false);
+    } else {
+      setClosed(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollPosition);
+    return function cleanup() {
+      window.removeEventListener('scroll', handleScrollPosition);
+    };
+  });
+
   return (
-    <header className="header">
+    <header 
+      className={classNames({
+        closed: closed,
+        open: !closed
+      })}
+    >
       <nav role="navigation" className="navbar">
         <Link className="navbar-link" to="/about">About Kei</Link>
         <Link className="navbar-link" to="/projects">Projects</Link>
